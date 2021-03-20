0d8b4faddb4bbad149974e08c630b3c51dd24ae3
nguyenhuutuan281 <78011126+nguyenhuutuan281@users.noreply.github.com>
2021-03-20 15:00:53 +0700

    Toado

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,42 @@
-package com.company;
+package OOP.Bai1;
 
-public class Main {
+public class Toado {
+    private int x, y;
+    private String name;
 
-    public static void main(String[] args) {
-        System.out.println("hoc ngu");
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Toado(int x, int y, String name) {
+        this.x = x;
+        this.y = y;
+        this.name = name;
+    }
+
+    public Toado() {}
+
+    public String toString(){
+        return name+ "("+x +"," + y +")";
     }
 }
